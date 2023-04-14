4af18106e4d6f7e0d633842d549dd7d7064d0084
Ravitej Varada <ravitejvarada@gmail.com>
2023-04-13 22:49:22 -0400

    first commit

---
+++ src/action.js
@@ -0,0 +1,50 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const { Version3Client } = require("jira.js");
+const fnTranslate = require("md-to-adf");
+
+const { issue } = github.context.payload;
+const hasLabel = issue.labels.find(
+  (label) => label.name === core.getInput("TRIGGER_LABEL")
+);
+
+const jiraClient = new Version3Client({
+  host: core.getInput("JIRA_API_HOST"),
+  authentication: {
+    basic: {
+      apiToken: core.getInput("JIRA_API_TOKEN"),
+      email: core.getInput("JIRA_EMAIL"),
+    },
+  },
+});
+
+async function createIssueInJIRA() {
+  if (!hasLabel) {
+    console.log("No trigger label detected. Skipping...");
+    return;
+  }
+
+  const descriptionBody = `View **${issue.title}** on [GitHub](${issue.html_url})
+---
+${issue.body}
+
+
+  `;
+
+  try {
+    await jiraClient.issues.createIssue({
+      fields: {
+        summary: `[GH] ${issue.title}`,
+        issuetype: {
+          name: core.getInput("JIRA_ISSUE_NAME"),
+        },
+        project: { key: core.getInput("JIRA_PROJECT_ID") },
+        description: fnTranslate(descriptionBody),
+      },
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+createIssueInJIRA();
